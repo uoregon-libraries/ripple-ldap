a6c8284f081771e2ab27ff54fe932dd9c850c876
Jeremy Echols <jechols@uoregon.edu>
2012-12-18 10:09:15 -0800

    Add skeleton test suite

---
+++ test/auth.js
@@ -0,0 +1,7 @@
+var libpath = process.env['PLUGIN_COV'] ? '../lib-cov' : '../lib';
+
+var should = require("should")
+  , sinon = require("sinon")
+
+describe("LDAP Authentication", function() {
+});
